40d7a611a1391b341b7cb169d97515abd30dc485
Maxime PIEDELEU <m.piedeleu.mp@gmail.com>
2025-09-04 17:11:26 +0200

    demarrage pahe sophro

--- src/app/Sophrologie/page.tsx
+++ src/app/Sophrologie/page.tsx
@@ -1,12 +1,25 @@
 import Header from '@/components/header/Header';
 import Coordonnees from '@/components/coordonnees/coordonnees';
 import Footer from '@/components/footer/footer';
+import Image from 'next/image';
+
 
 export default function Sophrologie() {
   return (
     <div className='font-sans'>
-      <Header />    
-      <div>Sophrologie</div>
+      <Header />
+      {/* Image en haut de page */}
+      <div className='relative h-64 md:h-80 w-full'>
+        <Image 
+          src="/images_site/sophrologie.webp" 
+          alt="Sophrologie" 
+          fill
+          className="object-cover object-center md:[object-position:center_20%]"
+        />
+      </div>
+      {/* Titre de la page */}
+      {/* paragraphe 1 */}
+      
        <Coordonnees variant="clair"/>
        <Footer variant="fonce"/>
     </div>
